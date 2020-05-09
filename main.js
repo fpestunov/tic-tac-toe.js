2c2b2c82666a42c29f88300dca75075550ed0b88
fpestunov <fpestunov@gmail.com>
2020-05-09 18:32:22 +0400

    step 3 - check a winner

--- main.js
+++ main.js
@@ -4,7 +4,17 @@ const replay = document.getElementById("replay");
 
 const players = ["O", "X"];
 let activePlayer = 0;
-const field = ["", "X", "", "O", "", "", "", "", ""];
+const field = ["", "", "", "X", "X", "X", "", "", ""];
+const winnersTable = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 
 function showResult(msg) {
   result.innerText = msg ? "Result: " + msg : msg;
@@ -21,6 +31,7 @@ board.addEventListener("click", function (event) {
     field[targetData.cell] = players[activePlayer];
     activePlayer = activePlayer ? 0 : 1;
     renderBoard();
+    checkWinner();
   }
 });
 
@@ -31,6 +42,19 @@ function renderBoard() {
     div.setAttribute("data-cell", index);
     div.innerText = field[index];
     board.append(div);
+  }
+}
+
+function checkWinner() {
+  for (let index = 0; index < winnersTable.length; index++) {
+    const el = winnersTable[index];
+    const isCellsEqual =
+      field[el[0]] === field[el[1]] &&
+      field[el[1]] === field[el[2]] &&
+      field[el[0]] !== "";
+    if (isCellsEqual) {
+      showResult(field[el[0]] + " win");
+    }
   }
 }
 
